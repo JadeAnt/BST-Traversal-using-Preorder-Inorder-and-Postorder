86a640b5b22c739ed787687204e8867a3c3e5f72
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-21 13:15:45 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct BSTNode{
+  int data;
+  struct BSTNode* left;
+  struct BSTNode* right;
+}
+
+void Preorder(BSTNode * root){
+  if(root == NULL)
+     return;
+  
+  printf("%d ", root->data);
+  Preorder(root->left);
+  Preorder(root->right);
+}
+
+void Inorder(BSTNode * root){
+  if(root == NULL)
+     return;
+  
+  Inorder(root->left);
+  printf("%d ", root->data);
+  Inorder(root->right);
+}
+
+void Postorder(BSTNode * root){
+  if(root == NULL)
+     return;
+  
+  Postorder(root->left);
+  Postorder(root->right);
+  printf("%d ", root->data);
+}
